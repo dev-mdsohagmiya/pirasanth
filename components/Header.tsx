ca7914d6df4cb78df6724e8efb0312f4f383db1d
ms-msbhh-ms <shohag.i.sajjad@gmail.com>
2023-07-11 17:58:26 +0600

    footer done

--- components/Header.tsx
+++ components/Header.tsx
@@ -91,17 +91,17 @@ export default function Header() {
             </button>
             {closeButton && (
               <button
-                className="outline-none mobile-menu-button absolute top-4  z-50"
+                className="outline-none mobile-menu-button absolute top-4   -translate-x-[230%] z-50"
                 type="button"
                 onClick={() => NavToggler()}
               >
-                <span className="cursor-pointer">
+                <span className="cursor-pointer text-white">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="50"
                     height="50"
-                    fill="currentColor"
-                    className="bi bi-x"
+                    fill="currentColor "
+                    className="bi bi-x text-white hover:text-primary"
                     viewBox="0 0 16 16"
                   >
                     <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
